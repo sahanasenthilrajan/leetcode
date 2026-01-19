fd27ef7acd153604a0e1490afb874b9e41262b39
sahanasenthilrajan <senthilsahana7@gmail.com>
2026-01-19 20:57:39 +0530

    Time: 0 ms (100%), Space: 42.9 MB (50.02%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -1,15 +1,16 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
-        int good=0;
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    good++;
+        int goodPairs = 0;
+        int[] counts = new int[101]; // Stores how many times we've seen each number
 
-                }
-            }
+        for (int num : nums) {
+            // 1. Add the number of times we've seen 'num' previously to our total
+            goodPairs += counts[num];
+            
+            // 2. Increment the count for 'num' because we just saw it again
+            counts[num]++;
         }
-        return good;
         
+        return goodPairs;
     }
 }
